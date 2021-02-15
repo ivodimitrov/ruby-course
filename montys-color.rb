b5044194f836eb6b3c300f4e6667796224667598
ivodimitrov <ivopdimitrov@gmail.com>
2021-02-15 19:10:36 +0200

    add if statement

--- montys-color.rb
+++ montys-color.rb
@@ -1,19 +1,19 @@
 # Lesson 3: Prompt for Favorite Color
 
-# puts "What is your favorite color?"
-# color = gets.chop
-# puts "Wow! My favorite color is #{color} also!"
+puts "What is your favorite color?"
+color = gets.chop
+puts "Wow! My favorite color is #{color} also!"
 
 # Lesson 4: Using an If Statement
 
-# if color == "blue"
-#     puts "Wow! My favorite color is #{color} also!"
-# end
+if color == "blue"
+  puts "Wow! My favorite color is #{color} also!"
+end
 
 # Lesson 5: Adding an Else Statement
 
-# if color == "blue"
-#     puts "Wow! My favorite color is #{color} also!"
-# else
-#     puts "I'm not a big fan of #{color}."
-# end
+if color == "blue"
+  puts "Wow! My favorite color is #{color} also!"
+else
+  puts "I'm not a big fan of #{color}."
+end
